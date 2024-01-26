23995c0f7fb81056625e94e684d649d7c9f0f283
Vasyl <vasyl@voicett.com>
2024-01-26 13:42:09 +0200

    test more cors.

--- backend/index.js
+++ backend/index.js
@@ -12,10 +12,22 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT;
 
+const whitelist = ['http://devops-exercise-11.s3-website-eu-west-1.amazonaws.com/']
+
+const corsOptions = {
+  origin: (origin, cb) => {
+    if(whitelist.indexOf(origin) > -1) {
+      cb(null, true);
+    } else {
+      cb(new Error('CORS forbidden'));
+    }
+  }
+}
+
 // serve react build
 app.use(express.static('build'));
 // mostly for local dev, since there is no reason to do this in prod, cause of same origin
-// app.use(cors());
+app.use(cors(corsOptions));
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
